bee14dd3d4d1e78ced2a28d3a00a1a789bac2005
qiukewei <kewei.qiu@mail.utoronto.ca>
2022-02-15 19:56:28 -0500

    update a5 files

--- a5/src/sphere.cpp
+++ a5/src/sphere.cpp
@@ -1,5 +1,6 @@
 #include "sphere.h"
 #include <iostream>
+#include <math.h>
 
 void sphere(
   const int num_faces_u,
@@ -15,13 +16,102 @@ void sphere(
   
   const double num_faces = num_faces_u * num_faces_v;
   const double num_vertices = num_faces_u * (num_faces_v - 1)+ 2;
+  const double radius = 1;
 
   V.resize(num_vertices, 3);
   F.resize(num_faces, 4);
-  UV.resize(num_vertices, 2);
+  UV.resize(num_faces_u * num_faces_v + num_faces_u + num_faces_v + 1, 2);
   UF.resize(num_faces, 4);
-  NV.resize(num_faces, 3);
+  NV.resize(num_vertices, 3);
   NF.resize(num_faces, 4);
+
+  // calculate V, UV, NV
+  int v_row = 0;
+  int uv_row = 0;
+
+  // 3d vertex on the south pole
+  V.row(v_row) << 0, 0, -radius;
+  v_row += 1;
+
+  // 2d coordinate on the botton of the texture
+  for (int i = 0; i < num_faces_u + 1; i++) {
+    UV.row(uv_row) << i / num_faces_u, 0;
+    uv_row += 1;
+  }
+
+  for (int v = 1; v < num_faces_v; v++) {
+    for (int u = 0; u < num_faces_u; u++) {
+      double theta = 2 * u * M_PI / num_faces_u;
+      double phi = v * M_PI / num_faces_v;
+      
+      double x = radius * cos(phi) * sin(theta);
+      double y = radius * sin(phi) * sin(theta);
+      double z = radius * cos(theta);
+
+      V.row(v_row) << x, y, z;
+      v_row += 1;
+
+      UV.row(uv_row) << u / num_faces_u, v / num_faces_v;
+      uv_row += 1;
+    }
+
+    // 2d coordinate on the right edge of the texture
+    UV.row(uv_row) <<  1, v / num_faces_v;
+    uv_row += 1;
+  }
+
+  // 3d vertex on the north pole
+  V.row(v_row) << 0, 0, radius;
+  v_row += 1;
+
+  // 2d coordinate on the top of the texture
+  for (int i = 0; i < num_faces_u + 1; i++) {
+    UV.row(uv_row) << i / num_faces_u, 1;
+    uv_row += 1;
+  }
+
+  NV = V.normalized();
+
+  // calculate F, UF, NF
+  int f_row = 0;
+  int uf_row = 0;
+  bool botton = false;
+  bool top = false;
+  bool right = false;
+
+  for (int v = 0; v < num_faces_v; v++) {
+    for (int u = 0; u < num_faces_u; u++) {
+      int botton_left = (v - 1) * (num_faces_u + 1) + u + 1;
+      int botton_right = (v - 1) * (num_faces_u + 1) + u + 2;
+      int top_left = v * (num_faces_u + 1) + u + 1;
+      int top_right = v * (num_faces_u + 1) + u + 2;
+
+      if (v == 0) {
+        botton_left = botton_right = 0;
+      }
+      if (v == num_faces_v - 1) {
+        top_left = top_right = V.rows() - 1;
+      }
+      if (u == num_faces_u - 1) {
+        botton_right -= u;
+        top_right -= u;
+      }
+
+      F.row(f_row) << botton_left, botton_right, top_right, top_left;
+      f_row += 1;
+
+      botton_left = v * (num_faces_u + 1) + u;
+      botton_right = v * (num_faces_u + 1) + u + 1;
+      top_left = (v + 1) * (num_faces_u + 1) + u;
+      top_right = (v + 1) * (num_faces_u + 1) + u + 1;
+
+      UF.row(uf_row) << botton_left, botton_right, top_right, top_left;
+      uf_row += 1;
+    }
+  }
+
+
+  NF = F;
 
   ////////////////////////////////////////////////////////////////////////////
 }
