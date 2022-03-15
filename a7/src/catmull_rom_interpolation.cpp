8e11a87daa8cf438f7e2e210736ffd8cbf7a2be1
yuqingc <yq822350@dal.ca>
2022-03-15 03:16:58 -0400

    update a7

--- a7/src/catmull_rom_interpolation.cpp
+++ a7/src/catmull_rom_interpolation.cpp
@@ -6,7 +6,73 @@ Eigen::Vector3d catmull_rom_interpolation(
   double t)
 {
   /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
-  return Eigen::Vector3d(0,0,0);
+  
+  // no keyframe, return zero vector
+  if (keyframes.size() == 0) {
+    return Eigen::Vector3d::Zero();
+  }
+
+  // one frame, image stay still
+  if (keyframes.size() == 1) {
+    return keyframes.front().second;
+  }
+
+  // calculate index of frame in a loop
+  double t_frame = keyframes.front().first + std::fmod(t, keyframes.back().first - keyframes.front().first);
+
+  // find the closest keyframe
+  int i;
+  for (i = 0; i < keyframes.size(); i++) {
+    if (keyframes[i].first > t_frame) {
+      break;
+    } else if (keyframes[i].first == t_frame) {
+      return keyframes[i].second;
+    }
+  }
+
+  // linear interpolation for boundaries
+  if (i < 2 || i > keyframes.size() - 2) {
+    double t0, t1;
+    t0 = keyframes[i-1].first;
+    t1 = keyframes[i].first;
+
+    Eigen::Vector3d theta0, theta1;
+    theta0 = keyframes[i-1].second;
+    theta1 = keyframes[i].second;
+
+    double T = (t_frame - t0) / (t1 - t0);
+    return theta0 + T * (theta1 - theta0);
+  }
+   
+  // get key-times about 4 keyframes
+  double t0, t1, t2, t3;
+  t0 = keyframes[i - 2].first;
+  t1 = keyframes[i - 1].first;
+  t2 = keyframes[i].first;
+  t3 = keyframes[i + 1].first;
+
+  // get correcponding key-values
+  Eigen::Vector3d theta0, theta1, theta2, theta3;
+  theta0 = keyframes[i - 2].second;
+  theta1 = keyframes[i - 1].second;
+  theta2 = keyframes[i].second;
+  theta3 = keyframes[i + 1].second;
+
+  // source: https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Catmull%E2%80%93Rom_spline
+  // calculate tangent of 2 pairs of keyframes
+  Eigen::Vector3d m1, m2;
+  m1 = (theta2 - theta0) / (t2 - t0);
+  m2 = (theta3 - theta1) / (t3 - t1);
+
+  // calculate interval substitute
+  double T = (t_frame - t1) / (t2 - t1);
+
+  // calculate c(t)
+  double T2 = T * T;
+  double T3 = T * T * T;
+  Eigen::Vector3d ct = (2 * T3 - 3 * T2 + 1) * theta1 + (T3 - 2 * T2 + T) * m1 + (-2 * T3 + 3 * T2) * theta2 + (T3 - T2) * m2;
+
+  return ct;
+
   /////////////////////////////////////////////////////////////////////////////
 }
--- a7/src/linear_blend_skinning.cpp
+++ a7/src/linear_blend_skinning.cpp
@@ -13,16 +13,17 @@ void linear_blend_skinning(
 
   for (int i = 0; i < V.rows(); i++) {
     Eigen::Vector3d vertex = V.row(i).transpose();
-
+    Eigen::Vector3d v = Eigen::Vector3d::Zero();
+    
     for (int j = 0; j < skeleton.size(); j++) {
-      if (W(i, skeleton[j].weight_index) > -1) {
-        vertex += T[j] * vertex * W(i, skeleton[j].weight_index);
+      if (skeleton[j].weight_index != -1) {
+        v += T[j] * vertex * W(i, skeleton[j].weight_index);
       }
     }
 
-    U(i, 0) = vertex(0);
-    U(i, 1) = vertex(1);
-    U(i, 2) = vertex(2);
+    U(i, 0) = v(0);
+    U(i, 1) = v(1);
+    U(i, 2) = v(2);
   }
 
   /////////////////////////////////////////////////////////////////////////////
